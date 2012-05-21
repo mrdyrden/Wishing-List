e66799fa951fa57fe3eb115a19e0d89d1afc7797
Ron <ronchipi@gmail.com>
2012-05-21 14:49:07 +0300

    Created heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WishingList::Application.config.secret_token = '7caf69b95f4d111f0826c4260131daca2f78b59bf2b4ba85881944d8bc7bf6d3bf4710df2892ed0ce69ca6fbbaf04c2b8ca0294928b949146042be02ca209caa'
